a80ef7f677d3fa2b363189fa77f60a019139901f
Belvatd <belva_talitha_28rpl@telkomschools.sch.id>
2022-03-02 18:24:09 +0700

    top menu done & progress carousell

--- src/components/fragments/CarouselContent/CarouselContent.js
+++ src/components/fragments/CarouselContent/CarouselContent.js
@@ -1,11 +1,55 @@
 import React from 'react';
-import './carouselContent.css';
+import { Card, Button } from 'react-bootstrap';
+import { Carousel } from '@trendyol-js/react-carousel';
 
 export default function CarouselContent() {
-  return (
-    <div className='red'>
-      CarouselContent
-    </div>
 
+  return (
+    <div>
+      <div className="carousel">
+        <Carousel show={1.5} slide={1} transition={0.5}>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                1
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                2
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                3
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                4
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Carousel>
+      </div>
+    </div>
   )
 }
--- src/components/fragments/TopMenu/TopMenu.js
+++ src/components/fragments/TopMenu/TopMenu.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import './topMenu.css';
+import image1 from '../../../images/top1.png';
+import image2 from '../../../images/top2.png';
+import image3 from '../../../images/top3.png';
 
 export default function TopMenu() {
   return (
-    <div className='redblu'>TopMenu</div>
+    <>
+      <div className="container-fluid my-5">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-9">
+            <div className="row justify-content-between align-items-center">
+              <div className="col-lg-7 col-sm-12">
+                <h1 className="textGreenb">Yayasan Al-uswah Islamiyah</h1>
+                <p>Seluruh komponen Lembaga Pendidikan islam Terpadu Al-Uswah Jatirogo 
+                  menggunakan kurikulum standar nasional berbasis kopetensi yaitu kurikulum JSIT & 
+                  Kurikulum 2013 yang merupakan penyempurnaan kurikulum yang bersifat inovatif dan 
+                  itergratif dengan paduan kurikulum khas Lembaga Pendidikan Islam Terpadu Al-Uswah 
+                  Jatirogo</p>
+              </div>
+              <div className="col-lg-5 col-sm-12">
+                <div className="rowContent">
+                  <img className="image1" src={image1} alt="image1" />
+                  <img className="image2 topImageLeft" src={image2} alt="image2" />
+                </div>
+                <div>
+                  <img className="image3 topImageTop" src={image3} alt="image3" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
