975a419357be60d86e988c3b01a0721303ab6443
Belvatd <belva_talitha_28rpl@telkomschools.sch.id>
2022-03-02 20:28:01 +0700

    update carousel 2

---
+++ src/components/fragments/CarouselContent/ArrowLeft.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import LeftArrow from '../../../images/leftArrow.png';
+
+export default function ArrowLeft() {
+  return (
+    <img src={LeftArrow} alt="leftarrow" />
+  )
+}
